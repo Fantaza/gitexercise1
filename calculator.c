2c2de0ca1da04648c2b269e9c7c91adbf4a559bf
Fantaza <72437912+Fantaza@users.noreply.github.com>
2020-11-05 11:34:17 +0100

    Calculator int und Switch case ändern
    int num2 in float num2 geändert 
    Switch case für die Operatoren (-,*,/) gemacht

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@
 int main(){
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
@@ -15,17 +15,26 @@ int main(){
 	printf("\nEnter two numbers: ");
 	scanf("%f",&num1);
 	scanf("%f",&num2);
-	
-	switch(op){
-		case '+' : 
-			ans = num1 + num2;
-			break;
-		default:
-			printf("Enter a correct operator!");
-			return 1;
-	
+
+	switch(op)
+    {
+
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",num1, num2, num1 / num2);
+            break;
+
 	}
 	printf("\n The result is as follows:\n");
 	printf("%f %c %f = %f",num1, op ,num2,ans);
 	return 0;
 }
+
